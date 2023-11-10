b110014affa648c6b14b0daebfb2dd9eca437f5a
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-11-10 23:41:45 +0600

    Home page done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from "react-router-dom";
-import myCreatedRouter from './Router/Route';
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={myCreatedRouter} />
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
