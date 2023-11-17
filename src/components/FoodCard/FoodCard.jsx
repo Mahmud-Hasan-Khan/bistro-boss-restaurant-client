b9ac712bc748073ffd93a073b9c35a59e75ec440
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-11-17 09:53:11 +0600

    cart done

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,79 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+// import axios from "axios";
+import { axiosSecure } from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
+
+const FoodCard = ({ item }) => {
+    const { name, image, price, recipe, _id } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate()
+    const location = useLocation();
+
+    const [, refetch] = useCart();
+
+    const handleAddToCart = () => {
+        if (user && user?.email) {
+            // Send cart item to the database
+            const cartItem = {
+                menuId: _id,
+                email: user?.email,
+                name,
+                image,
+                price
+            }
+            axiosSecure.post('/carts', cartItem)
+                .then(res => {
+                    console.log(res.data);
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-center",
+                            icon: "success",
+                            title: `${name} added to cart`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        // refetch cart to update the cart item
+                        refetch();
+                    }
+                })
+
+        } else {
+            Swal.fire({
+                title: "You are not logged In",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login Now!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                    // Swal.fire({
+                    //     title: "Deleted!",
+                    //     text: "Your file has been deleted.",
+                    //     icon: "success"
+                    // });
+                }
+            });
+        }
+    }
+
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Shoes" /></figure>
+            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
+            <div className="card-body flex flex-col items-center">
+                <h2 className="card-title">{name}</h2>
+                <p>{recipe}</p>
+                <div className="card-actions justify-end">
+                    <button onClick={handleAddToCart} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,19 +1,42 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const [data] = useCart();
+    console.log(data);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error));
+    }
 
     const navOptions = <>
-        <li><a>Item 555</a></li>
-        <li tabIndex={0}>
-            <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-            </a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
+        <li><Link to="/secret">Secret</Link></li>
+        <li>
+            <Link to="/">
+                <button className="btn">
+                    <FaShoppingCart className="mr-2" />
+                    <div className="badge badge-secondary">{data.length} </div>
+                </button>
+            </Link>
+
         </li>
-        <li><a>Item 3</a></li>
+        {
+            user ? <>
+                <span>{user?.displayName}</span>
+                <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
+            </> : <>
+                <li><Link to="/login">Login</Link></li>
+            </>
+        }
     </>
 
     return (
